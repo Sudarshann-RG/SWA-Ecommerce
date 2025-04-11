2d1f89aa52d2c6b816e72d0a8c2fc26c68f9a021
Sudarshann-RG <sudarshann@docu3c.com>
2025-04-11 20:52:30 +0530

    Update CreateItemsPage.jsx

--- src/pages/CreateItemsPage.jsx
+++ src/pages/CreateItemsPage.jsx
@@ -11,7 +11,7 @@ const CreateItemsPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await fetch('/api/createItem', {
+      await fetch('/api/items', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
